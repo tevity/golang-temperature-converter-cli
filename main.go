e10cd22252fe33c054b413d4e39b137f8d5b45c1
Kevin Gurton <kevin.gurton@gmail.com>
2020-05-06 15:49:43 +0100

    Read originValue

--- main.go
+++ main.go
@@ -26,6 +26,10 @@ func main() {
 
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+		_, err = fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
