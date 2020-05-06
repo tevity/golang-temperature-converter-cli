3c0d00302b3a7f0e960e7ff55ac32343654f6b88
Kevin Gurton <kevin.gurton@gmail.com>
2020-05-06 15:54:52 +0100

    Read in convert again

--- main.go
+++ main.go
@@ -38,7 +38,11 @@ func main() {
 		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanln(&shouldConvertAgain)
 
+		if err != nil {
+			printError(errReadingInput)
+		}
 		if shouldConvertAgain != "Y" {
 			fmt.Println("Good bye!")
 			break
